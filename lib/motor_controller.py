75113d0c6896593e90cdb682507c1c3ddda08cdb
eduardohufg <eduardochavezmartin10@gmail.com>
2025-03-01 16:52:54 -0600

    Add: ros node

---
+++ lib/motor_controller.py
@@ -0,0 +1,108 @@
+import serial
+import time
+
+class MotorController:
+    def __init__(self, port, baudrate=115200) -> bool:
+
+        """
+        Initialize the motor controller object
+        :param port: Serial port to connect to the motor controller
+        :param baudrate: Baudrate of the serial port
+        """
+
+        self.port = port
+        self.baudrate = baudrate
+        try:
+            self.uart = serial.Serial(self.port, self.baudrate, timeout=1)
+            time.sleep(2)
+            return True
+        except serial.SerialException as e:
+            self.uart = None
+            return False
+
+    def __del__(self) -> None:
+        if self.uart is not None:
+            commad = "STOP\n"
+            self.uart.write(commad.encode())
+            self.uart.close()
+
+    def init_motor(self) -> bool:
+        if self.uart is not None:
+            self.uart.write("INIT\n".encode())
+            return True
+        else:
+            return False
+        
+    def close_motor(self) -> bool:
+        if self.uart is not None:
+            self.uart.write("STOP\n".encode())
+            self.uart.close()
+            return True
+        else:
+            return False
+        
+    def motor_mode(self, mode) -> bool:
+        if self.uart is not None:
+            command = f"MODE,{mode}\n"
+            self.uart.write(command.encode())
+            return True
+        else:
+            return False
+
+    def set_pid_gains(self, kp, ki, kd) -> bool:
+        if self.uart is not None:
+            command = f"PID,{kp},{ki},{kd}\n"
+            self.uart.write(command.encode())
+            return True
+        else:
+            return False
+        
+    def set_setpoint(self, setpoint) -> bool:
+        if self.uart is not None:
+            command = f"SP,{setpoint}\n"
+            self.uart.write(command.encode())
+            return True
+        else:
+            return False
+        
+    def get_position(self) -> str:
+        if self.uart is not None:
+            self.uart.write("GET_POS\n".encode())
+            return self.uart.readline().decode().strip()
+        
+    def get_velocity(self) -> str:
+        if self.uart is not None:
+            self.uart.write("GET_VEL\n".encode())
+            return self.uart.readline().decode().strip()
+        
+    def set_windup_limit(self, limit) -> bool:
+        if self.uart is not None:
+            command = f"LIMIT,{limit}\n"
+            self.uart.write(command.encode())
+            return True
+        else:
+            return False
+        
+    def set_motor_direction(self, direction) -> bool:
+        if self.uart is not None:
+            command = f"DIRECTION,{direction}\n"
+            self.uart.write(command.encode())
+            return True
+        else:
+            return False
+        
+    def set_sensor_phase(self, phase) -> bool:
+        if self.uart is not None:
+            command = f"PHASE,{phase}\n"
+            self.uart.write(command.encode())
+            return True
+        else:
+            return False
+        
+    def set_saturation(self, saturation) -> bool:
+        if self.uart is not None:
+            command = f"SAT,{saturation}\n"
+            self.uart.write(command.encode())
+            return True
+        else:
+            return False
---
+++ motor_gen_control/motor_control.py
@@ -0,0 +1,154 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+from lib.motor_controller import MotorController
+from rcl_interfaces.msg import SetParametersResult
+import signal
+import sys
+
+def my_map(value: float, in_min: float, in_max,out_min: float, out_max: float) -> float:
+
+    targetPos: float = (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return targetPos
+
+def get_min_max_encoder_pos(zero_encoder_pos: float, min_angle_pos: float, max_angle_pos: float) -> tuple:
+    current_180 = 2011.0
+
+    min_encoder_pos: float = min_angle_pos * current_180 / 180.0 + zero_encoder_pos
+    max_encoder_pos: float = max_angle_pos * current_180 / 180.0 + zero_encoder_pos
+
+    return min_encoder_pos, max_encoder_pos
+        
+class MotorControl(Node):
+    def __init__(self):
+        super().__init__('motor_control')
+
+        self.declare_parameter('port', '/dev/ttyTHS1')
+        self.declare_parameter('kp', 1.1)
+        self.declare_parameter('ki', 1.1)
+        self.declare_parameter('kd', 0.005)
+        self.declare_parameter('min_angle', -90.0)
+        self.declare_parameter('max_angle', 90.0)
+        self.declare_parameter('zero_encoder_pos', 2011.0)
+        
+        self.port = self.get_parameter('port').get_parameter_value().string_value
+        self.kp = self.get_parameter('kp').get_parameter_value().double_value
+        self.ki = self.get_parameter('ki').get_parameter_value().double_value   
+        self.kd = self.get_parameter('kd').get_parameter_value().double_value
+        self.min_angle = self.get_parameter('min_angle').get_parameter_value().double_value
+        self.max_angle = self.get_parameter('max_angle').get_parameter_value().double_value
+        self.zero_encoder_pos = self.get_parameter('zero_encoder_pos').get_parameter_value().double_value
+
+        self.min_encoder_pos, self.max_encoder_pos = get_min_max_encoder_pos(self.zero_encoder_pos, self.min_angle, self.max_angle)
+
+        self.create_subscription(Float64, '/arm_teleop/joint2', self.callback_joint2, 10)
+
+        self.add_on_set_parameters_callback(self.parameter_callback)    
+        self.angle_joint2: float = 0.0
+
+        self.motor = MotorController(self.port, 1000000)
+        self.motor.motor_mode(0)
+        self.motor.set_pid_gains(self.kp, self.ki, self.kd)
+        self.motor.set_windup_limit(20)
+    
+        if self.motor.init_motor():
+            self.get_logger().info(f"Motor controller initialized on port: {self.port}")
+        else:
+            self.get_logger().info(f"Failed to initialized motor on pott: {self.port}")
+
+        signal.signal(signal.SIGINT, self.close_uart)
+
+    def parameter_callback(self, params) -> None:
+        
+        for param in params:
+            if param.name == 'kp':
+
+                if param.value < 0.0:
+                    self.get_logger().warn("kp cannot be negative")
+                    return SetParametersResult(successful=False, reason="kp cannot be negative")
+                else:
+                    self.kp = param.value.double_value
+                    self.motor.set_pid_gains(self.kp, self.ki, self.kd)
+                    self.get_logger().info(f"New kp value: {self.kp}")
+            elif param.name == 'ki':
+
+                if param.value < 0.0:
+                    self.get_logger().warn("ki cannot be negative")
+                    return SetParametersResult(successful=False, reason="ki cannot be negative")
+                else:
+                    self.ki = param.value.double_value
+                    self.motor.set_pid_gains(self.kp, self.ki, self.kd)
+                    self.get_logger().info(f"New ki value: {self.ki}")
+            elif param.name == 'kd':
+                
+                if param.value < 0.0:
+                    self.get_logger().warn("kd cannot be negative")
+                    return SetParametersResult(successful=False, reason="kd cannot be negative")
+                else:
+                    self.kd = param.value.double_value
+                    self.motor.set_pid_gains(self.kp, self.ki, self.kd)
+                    self.get_logger().info(f"New kd value: {self.kd}")
+
+            elif param.name == 'min_angle':
+                if self.min_angle < -180.0:
+                    self.get_logger().warn("min_angle cannot be less than -180.0")
+                    return SetParametersResult(successful=False, reason="min_angle cannot be less than -180.0")
+                else:
+                    self.min_angle = param.value.double_value
+                    self.min_encoder_pos, self.max_encoder_pos = get_min_max_encoder_pos(self.zero_encoder_pos, self.min_angle, self.max_angle)
+                    self.get_logger().info(f"New min_angle value: {self.min_angle}")
+
+            elif param.name == 'max_angle':
+
+                if self.max_angle > 180.0:
+                    self.get_logger().warn("max_angle cannot be greater than 180.0")
+                    return SetParametersResult(successful=False, reason="max_angle cannot be greater than 180.0")
+                else:
+
+                    self.max_angle = param.value.double_value
+                    self.min_encoder_pos, self.max_encoder_pos = get_min_max_encoder_pos(self.zero_encoder_pos, self.min_angle, self.max_angle)
+                    self.get_logger().info(f"New max_angle value: {self.max_angle}")
+
+            elif param.name == 'zero_encoder_pos':
+                self.zero_encoder_pos = param.value.double_value
+                self.min_encoder_pos, self.max_encoder_pos = get_min_max_encoder_pos(self.zero_encoder_pos, self.min_angle, self.max_angle)
+                self.get_logger().info(f"New zero_encoder_pos value: {self.zero_encoder_pos}")
+
+    def close_uart(self, sig, frame) -> None:
+        if self.motor:
+            self.motor.close_motor()
+        sys.exit(0)
+
+    def callback_joint2(self, msg: Float64) -> None:
+        self.angle_joint2 = float(msg.data)
+        self.joint_arm_2()
+
+    def joint_arm_2(self) -> None:
+
+        if self.motor is None:
+            return
+        
+        if(self.angle_joint2 >= self.min_angle and self.angle_joint2 <= self.max_angle):
+
+            targetPos = my_map(self.angle_joint2, self.min_angle, self.max_angle, self.min_encoder_pos, self.max_encoder_pos)
+            self.motor.set_setpoint(targetPos)
+
+        else:
+            self.get_logger().info(f"Angle out of range: {self.angle_joint2}")
+            
+def main(args=None):
+    rclpy.init(args=args)
+    motor_control = MotorControl()
+    try:
+        rclpy.spin(motor_control)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        motor_control.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
+    
